53539a1cc46f40d931d4bd49ef9359227132378b
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-04 18:58:43 +0900

    Title: 등차수열, Time: 76ms, Memory: 18MB, Status: Passed - CodeTree

--- 250304/등차수열/arithmetic-sequence.py
+++ 250304/등차수열/arithmetic-sequence.py
@@ -3,11 +3,16 @@ a = list(map(int, input().split()))
 
 # Please write your code here.
 max_a = max(a)
+min_a = min(a)
 
-cnt = 0
-for N in range(1, max(a)):
+max_cnt = 0
+for N in range(min_a, max_a):
+    cnt = 0
     for j in range(len(a)):
-        if abs(a[j] - N) == abs(a[j - 1] - N):
-            cnt += 1
-
-print(cnt)
+        for k in range(len(a)):
+            if j == k:
+                continue
+            if abs(a[j] - N) == abs(a[k] - N):
+                cnt += 1
+    max_cnt = max(max_cnt, cnt)
+print(int(max_cnt/2))
