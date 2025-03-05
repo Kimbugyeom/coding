22eebe2e66d33458ec3e3fc4a2d3f7bd3cc0f84f
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-05 18:14:29 +0900

    Title: 팀으로 하는 틱택토 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250305/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 250305/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -4,30 +4,45 @@ max_num = 9
 cnt = 0
 
 for i in range(1, max_num + 1):
-    for j in range(1, max_num + 1):
+    for j in range(i + 1, max_num + 1):
         if i == j:
             continue
 
+        found = False  # j 루프 시작 시 초기화
+
+        # 가로 체크
         for k in range(3):
-            if not (inp[k][0] == i and inp[k][1] == i and inp[k][2] == i) and \
-               not (inp[k][0] == j and inp[k][1] == j and inp[k][2] == j) and \
-               (inp[k][0] in [i, j] and inp[k][1] in [i, j] and inp[k][2] in [i, j]):
-                cnt += 1
+            if (inp[k][0] in [i, j] and inp[k][1] in [i, j] and inp[k][2] in [i, j]) and \
+               not (inp[k][0] == i and inp[k][1] == i and inp[k][2] == i) and \
+               not (inp[k][0] == j and inp[k][1] == j and inp[k][2] == j):
+                found = True
+                break
 
-        for k in range(3):
-            if not (inp[0][k] == i and inp[1][k] == i and inp[2][k] == i) and \
-               not (inp[0][k] == j and inp[1][k] == j and inp[2][k] == j) and \
-               (inp[0][k] in [i, j] and inp[1][k] in [i, j] and inp[2][k] in [i, j]):
-                cnt += 1
+        # 세로 체크
+        if not found:
+            for k in range(3):
+                if (inp[0][k] in [i, j] and inp[1][k] in [i, j] and inp[2][k] in [i, j]) and \
+                   not (inp[0][k] == i and inp[1][k] == i and inp[2][k] == i) and \
+                   not (inp[0][k] == j and inp[1][k] == j and inp[2][k] == j):
+                    found = True
+                    break
 
-        if not (inp[0][0] == i and inp[1][1] == i and inp[2][2] == i) and \
-           not (inp[0][0] == j and inp[1][1] == j and inp[2][2] == j) and \
-           (inp[0][0] in [i, j] and inp[1][1] in [i, j] and inp[2][2] in [i, j]):
+        # 대각선 체크
+        if not found:
+            if (inp[0][0] in [i, j] and inp[1][1] in [i, j] and inp[2][2] in [i, j]) and \
+               not (inp[0][0] == i and inp[1][1] == i and inp[2][2] == i) and \
+               not (inp[0][0] == j and inp[1][1] == j and inp[2][2] == j):
+                found = True
+
+        if not found:
+            if (inp[0][2] in [i, j] and inp[1][1] in [i, j] and inp[2][0] in [i, j]) and \
+               not (inp[0][2] == i and inp[1][1] == i and inp[2][0] == i) and \
+               not (inp[0][2] == j and inp[1][1] == j and inp[2][0] == j):
+                found = True
+
+        if found:
             cnt += 1
+            break 
 
-        if not (inp[0][2] == i and inp[1][1] == i and inp[2][0] == i) and \
-           not (inp[0][2] == j and inp[1][1] == j and inp[2][0] == j) and \
-           (inp[0][2] in [i, j] and inp[1][1] in [i, j] and inp[2][0] in [i, j]):
-            cnt += 1
+print(cnt)
 
-print(cnt//2)
