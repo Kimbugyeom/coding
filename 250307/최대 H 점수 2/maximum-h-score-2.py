de00e1e6a305e716a122e88ffdce0ba37ec1f4f8
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-07 22:14:01 +0900

    Title: 최대 H 점수 2, Time: 52ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250307/최대 H 점수 2/maximum-h-score-2.py
@@ -0,0 +1,24 @@
+N, L = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(set(a))
+b.sort()
+# Please write your code here.
+my_list = []
+for elem in b:
+    dist = elem
+    cnt = 0
+    using_L = L
+    for i in range(len(a)):
+        if a[i] >= dist:
+            cnt += 1
+        else:
+            if using_L >= 1:
+                if a[i] + 1 >= dist:
+                    cnt += 1
+                    using_L -= 1
+
+    if cnt >= dist:
+        my_list.append(dist)
+
+print(max(my_list))
+    
