bb1b67d16e712eb9e8282dd892acade238aac409
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-07 23:50:23 +0900

    Title: 코딩톡, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250307/코딩톡/coding-talk.py
+++ 250307/코딩톡/coding-talk.py
@@ -10,11 +10,13 @@ for i in range(n):
 read_list = []
 
 for i in range(m):
-    user, num = c[i], u[i]
     if i >= p - 1:
-        read_list.append(user)
+        read_list.append(c[i])
     if p - 1 == 0:
         read_list = user_list
+    if i < p - 1:
+        if u[p - 1] == u[i]:
+            read_list.append(c[i])
 
 for elem in user_list:
     if elem not in read_list:
