f02356a762d72ee6455c3b486e7954bf3c9aed95
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-07 22:40:36 +0900

    Title: 구역 청소, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250307/구역 청소/district-cleaning.py
+++ 250307/구역 청소/district-cleaning.py
@@ -4,7 +4,9 @@ c, d = map(int, input().split())
 # Please write your code here.
 if b < c or d < a:
     print((b - a) + (d - c))
-elif b >= c and a > d:
+elif a <= c <= b <= d:
     print(d - a)
-elif a <= d and b >= c:
+elif c <= a <= d <= b:
     print(b - c)
+else:
+    print(max(a,b,c,d) - min(a,b,c,d))
