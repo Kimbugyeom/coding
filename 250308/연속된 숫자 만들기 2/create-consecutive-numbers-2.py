6c48dfabea42d5accf2931be8173c1d92985140c
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-08 21:58:37 +0900

    Title: 연속된 숫자 만들기 2, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250308/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 250308/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -8,5 +8,9 @@ if x1 + 1 == x2 and x2 + 1 == x3:
     print(0)
 elif x3 + 1 == x2 and x2 + 1 == x1:
     print(0)
+elif x2 - 1 <= x1 <= x2 + 1 or x2 - 1 <= x3 <= x2 + 1:
+    print(2)
+elif x2 - 2 <= x1 <= x2 + 2 or x2 - 2 <= x3 <= x2 + 2:
+    print(1)
 else:
     print(2)
