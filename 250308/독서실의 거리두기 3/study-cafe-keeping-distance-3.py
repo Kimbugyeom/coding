8c929fe76856b8e06529f93861d283f91069ccf3
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-08 22:36:42 +0900

    Title: 독서실의 거리두기 3, Time: 371ms, Memory: 21MB, Status: Passed - CodeTree

---
+++ 250308/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,19 @@
+N = int(input())
+seat = list(input())
+
+ans = 0
+for i in range(N - 1):
+    if seat[i] == '0':
+        seat[i] = '1'
+
+        min_dist = N 
+        for k in range(N - 1):
+            for w in range(k + 1, N):
+                if seat[k] == '1' and seat[w] == '1':
+                    dist = abs(w - k)
+                    min_dist = min(min_dist, dist)
+        ans = max(ans, min_dist)
+
+        seat[i] = '0'
+
+print(ans)
