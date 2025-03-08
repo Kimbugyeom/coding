f8a436315eb17d43cb6072a0ac90b55078ccdf20
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-08 22:03:48 +0900

    Title: 연속된 숫자 만들기 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250308/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 250308/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -3,10 +3,16 @@ pos = list(map(int, input().split()))
 # Please write your code here.
 x1, x2, x3 = pos[0], pos[1], pos[2]
 
+546
+465
 
 if x1 + 1 == x2 and x2 + 1 == x3:
     print(0)
 elif x3 + 1 == x2 and x2 + 1 == x1:
+    print(0)
+elif x2 + 1 == x1 and x1 + 1 == x3:
+    print(0)
+elif x1 + 1 == x3 and x3 + 1 == x2:
     print(0)
 elif x2 - 1 <= x1 <= x2 + 1 or x2 - 1 <= x3 <= x2 + 1:
     print(2)
