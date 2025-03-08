5b650e916ff70c209e938ba8f11ac82bb8f11cac
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-08 21:36:51 +0900

    Title: 순 간 이 동, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250308/순 간 이 동/teleportation.py
@@ -0,0 +1,15 @@
+a, b, x, y = map(int, input().split())
+
+# Please write your code here.
+dist = []
+
+case1 = b - a
+dist.append(case1)
+
+case2 = abs(a - x) + abs(b- y)
+dist.append(case2)
+
+case3 = abs(a - y) + abs(b - x)
+dist.append(case3)
+
+print(min(dist))
