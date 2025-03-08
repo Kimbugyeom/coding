a8809b011213e74ecbb6b20fde2deb33774affb1
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-08 21:31:13 +0900

    Title: X 달리기, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250308/X 달리기/x-run.py
+++ 250308/X 달리기/x-run.py
@@ -4,7 +4,7 @@ X = int(input())
 speed = 1
 dist = 1
 time = 1
-
+cnt = 0
 while dist < X:
     if dist <= X // 2:
         speed += 1
@@ -16,9 +16,13 @@ while dist < X:
             speed -= 1
         else:
             speed = 1
+            cnt += 1
         dist += speed
         time += 1
+
 if speed > 1 :
     print(time + speed - 1)
+elif speed == 1 and cnt > 1:
+    print(time - cnt + 1)
 else:
     print(time)
