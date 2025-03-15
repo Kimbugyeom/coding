7c8a55b843d22d8fd2cc67d40d067ecd69f0c33f
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-16 01:34:22 +0900

    Title: 세 수의 최대 곱, Time: 67ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250316/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 250316/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -5,11 +5,36 @@ arr = list(map(int, input().split()))
 import sys
 
 max_num = -sys.maxsize
-
+plus = []
+minus = []
+zero = []
 for i in range(n):
-    for j in range(i + 1, n):
-        for k in range(j + 1, n):
-            if arr[i] * arr[j] * arr[k] > max_num:
-                max_num = arr[i] * arr[j] * arr[k]
+    if arr[i] > 0:
+        plus.append(arr[i])
+    elif arr[i] < 0:
+        minus.append(arr[i])
+    else:
+        zero.append(arr[i])
+plus.sort(reverse = True)
+minus.sort()
+
+#  + + +
+max_plus = plus[0] * plus[1] * plus[2]
+# - - +
+max_minus = plus[0] * minus[0] * minus[1]
+
+if len(plus) >= 3:
+    max_plus = plus[0] * plus[1] * plus[2]
+    if len(minus) >= 2:
+        max_minus = plus[0] * minus[0] * minus[1]
+    else:
+        max_minus = 0
+    max_num = max(max_plus, max_minus)
+elif len(minus) >= 2:
+       max_plus = 0
+       max_minus = plus[0] * minus[0] * minus[1]
+       max_num = max(max_plus, max_minus)
+else:
+    max_num = 0
 
 print(max_num)
