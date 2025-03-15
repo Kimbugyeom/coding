f30dd1b762e9d8ce8aa5b3042db45e0df7077484
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-15 21:55:54 +0900

    Title: L, R 그리고 B, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250315/L, R 그리고 B/l-r-and-b.py
+++ 250315/L, R 그리고 B/l-r-and-b.py
@@ -5,6 +5,8 @@ B_x = 0
 B_y = 0
 L_x = 0
 L_y = 0
+R_x = 0
+R_y = 0
 for i in range(10):
     for j in range(10):
         if board[i][j] == 'B':
@@ -13,7 +15,14 @@ for i in range(10):
         if board[i][j] == 'L':
             L_x = i
             L_y = j
+        if board[i][j] == 'R':
+            R_x = i
+            R_y = j
 
 B = B_x + B_y
 L = L_x + L_y
-print(abs(B - L) - 1)
+
+if B_x == R_x == L_x or L_y == B_y == R_y:
+    print(abs(B - L) + 1)
+else:
+    print(abs(B - L) - 1)
