d2d78a0ccb52c26173b14503b0db26a3f61f6b3a
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-16 18:57:50 +0900

    Title: 세 수의 최대 곱, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250316/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 250316/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,35 +1,43 @@
+import sys
+
 n = int(input())
 arr = list(map(int, input().split()))
-
-# Please write your code here.
-import sys
 
 max_num = -sys.maxsize
 plus = []
 minus = []
-zero = []
-for i in range(n):
-    if arr[i] > 0:
-        plus.append(arr[i])
-    elif arr[i] < 0:
-        minus.append(arr[i])
+zero = False
+
+for num in arr:
+    if num > 0:
+        plus.append(num)
+    elif num < 0:
+        minus.append(num)
     else:
-        zero.append(arr[i])
-plus.sort(reverse = True)
+        zero = True
+
+plus.sort(reverse=True)
 minus.sort()
 
-if len(plus) >= 3:
-    max_plus = plus[0] * plus[1] * plus[2]
-    if len(minus) >= 2:
-        max_minus = plus[0] * minus[0] * minus[1]
-    else:
-        max_minus = 0
-    max_num = max(max_plus, max_minus)
-elif len(minus) >= 2:
-       max_plus = 0
-       max_minus = plus[0] * minus[0] * minus[1]
-       max_num = max(max_plus, max_minus)
-else:
-    max_num = 0
+candidates = []
 
-print(max_num)
+if len(plus) >= 3:
+    candidates.append(plus[0] * plus[1] * plus[2])
+
+if len(plus) >= 1 and len(minus) >= 2:
+    candidates.append(plus[0] * minus[0] * minus[1])
+
+if len(minus) >= 3:
+    candidates.append(minus[-1] * minus[-2] * minus[-3])
+
+if len(minus) >= 2 and len(plus) == 1:
+    candidates.append(plus[0] * minus[-1] * minus[-2])
+
+if len(plus) >= 2 and len(minus) == 1:
+    candidates.append(plus[-1] * plus[-2] * minus[0])
+
+if zero:
+    candidates.append(0)
+
+print(max(candidates))
+
