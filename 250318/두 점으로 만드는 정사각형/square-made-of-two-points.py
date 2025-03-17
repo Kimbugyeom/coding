3941fbabf56358bcf23146e526de0623d088ebc4
BuGyeom <128015407+Kimbugyeom@users.noreply.github.com>
2025-03-18 00:43:59 +0900

    Title: 두 점으로 만드는 정사각형, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250318/두 점으로 만드는 정사각형/square-made-of-two-points.py
@@ -0,0 +1,16 @@
+x1, y1, x2, y2 = map(int, input().split())
+a1, b1, a2, b2 = map(int, input().split())
+
+# Please write your code here.
+new_x1 = min(x1, a1)
+new_y1 = min(y1, b1)
+new_a1 = max(x2, a2)
+new_b1 = max(y2, b2)
+
+x = abs(new_a1 - new_x1)
+y = abs(new_b1 - new_y1)
+
+if x > y:
+    print(x * x)
+else:
+    print(y * y)
